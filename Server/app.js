73da46610650a572be10bfcc6ddd41a21768dcda
mycoal2 <michaelhong136@gmail.com>
2023-02-17 20:16:31 -0500

    implemented database

--- Server/app.js
+++ Server/app.js
@@ -1,25 +1,40 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-const mysql = require("mysql");
+// const bodyParser = require("body-parser");
+const mysql = require('mysql');
 const cors = require("cors");
 
-const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "weebdb",
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'weeb_db'
 });
+
 app.use(cors());
 app.use(express.json());
 // app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/signup', (req,res) => {
-    
+db.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + db.threadId);
+});
+
+app.get("/account", (req, res) => {
+    const sqlSelect = "SELECT * FROM accounts";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    })
+})
+
+app.post("/signup", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    const sqlInsert = "INSERT INTO Accounts (username, password) VALUES (?,?)";
+    const sqlInsert = "INSERT INTO accounts (username, password) VALUES (?,?)";
     db.query(sqlInsert, [username, password], (err, result) => {
         if(err) {
             console.log(err);
@@ -27,7 +42,6 @@ app.post('/signup', (req,res) => {
             res.send("Values inserted");
         }
     });
-    res.send("hello world")
 });
 
 app.listen(3001, () => {
